2d7f57c7fd45eb71f2c6411a87f3a7df242796f4
Nathan Corry <32592701+Wander1ust@users.noreply.github.com>
2019-01-25 18:45:32 +0000

    arc

--- sketch.js
+++ sketch.js
@@ -1,12 +1,64 @@
+let numbers = [true];
+let sequence = [];
+let count = 1;
+let index = 0;
 
+let arcs = [];
+
+class Arc {
+    constructor(start, end, dir) {
+        this.start = start;
+        this.end = end;
+        this.dir = dir;
+    }
+
+    show() {
+
+        let diameter = abs(this.end - this.start);
+        let x = (this.end + this.start) / 2;
+        stroke(255);
+        noFill();
+        if (this.dir == 0) {
+            arc(x, height / 2, diameter, diameter, PI, 0);
+        } else {
+            arc(x, 0, diameter, diameter, 0, PI);
+        }
+    }
+}
 
 function setup() {
-        createCanvas(windowWidth, windowHeight);
-        background(255, 0, 200);
-      }
+    createCanvas(windowWidth, windowHeight);
+    background(0);
+    numbers[index] = true;
+    sequence.push(index);
+
+}
+
+function step() {
+    let next = index - count;
+    if (next < 0 || numbers[next]) {
+        next = index + count;
+    }
+    numbers[next] = true;
+    sequence.push(next);
+
+    let a = new Arc(index, next, count % 2);
+    arcs.push(a);
+
+    index = next;
+    count++;
+
+}
+
 
 
 function draw() {
+    step();
+  //  translate(0, height / 2);
+    scale(width / count);
     background(0);
 
-        }
+    for(let a of arcs) {
+        a.show();
+    }
+}
